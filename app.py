4fe74b8cd2d1458441dacae96b39a5bb3c0eb989
Smrithi Raj <smrithi.raj27@gmail.com>
2026-03-31 20:57:00 +0530

    Added deployment files

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = '/tmp'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
